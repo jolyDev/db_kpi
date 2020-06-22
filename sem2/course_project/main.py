42dcd9f0f886b23b68529da209677ac6375ce0c7
jolyDev <vernyhora.artem@gmail.com>
2020-06-22 15:05:13 +0300

    restructuring

--- sem2/course_project/main.py
+++ sem2/course_project/main.py
@@ -1,4 +1,19 @@
 import ui
 
+print('''
+[0.............................100%]
+*.py files
+732 lines
+
+*.html files
+16.692 lines
+
+*.tcl files
+511 lines
+
+*.* others
+138 lines
+''')
+
 if __name__ == '__main__':
     ui.menu()
--- sem2/course_project/ui.py
+++ sem2/course_project/ui.py
@@ -58,4 +58,3 @@ def menu():
         return
 
     menu()
-
